23e723faa1cccd07fcd810f02b7416519536ef38
relaxcore <yuriirogulya@gmail.com>
2019-05-26 20:37:46 +0300

    seed file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,58 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     TestTask.Repo.insert!(%TestTask.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias TestTask.Repo
+alias TestTask.ShopList.{Category, Item}
+
+Repo.delete_all(Item)
+Repo.delete_all(Category)
+
+categories = [
+  %Category{name: "Groceries"},
+  %Category{name: "Beverages"},
+  %Category{name: "Personal Care"}
+]
+
+Enum.each(categories, fn(data) ->
+  Repo.insert! data
+end)
+
+############
+
+beverages_id     = Repo.get_by!(Category, name: "Beverages").id
+groceries_id     = Repo.get_by!(Category, name: "Groceries").id
+personal_care_id = Repo.get_by!(Category, name: "Personal Care").id
+
+items = [
+  %Item{
+    name: "Coca-Cola",
+    price: 2.5,
+    category_id: beverages_id
+  },
+  %Item{
+    name: "Beer",
+    price: 3.25,
+    category_id: beverages_id
+  },
+  %Item{
+    name: "Rice",
+    price: 5.0,
+    category_id: groceries_id
+  },
+  %Item{
+    name: "Barbecue sauce",
+    price: 1.5,
+    category_id: groceries_id
+  },
+  %Item{
+    name: "Shampoo",
+    price: 7.0,
+    category_id: personal_care_id
+  },
+  %Item{
+    name: "Toothbrush",
+    price: 2.5,
+    category_id: personal_care_id
+  }
+]
+
+Enum.each(items, fn(data) ->
+  Repo.insert! data
+end)
